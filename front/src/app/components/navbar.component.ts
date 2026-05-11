4efca775a185d9d9c4ddcb2cea7845056ba44d41
rafael <estagio.cirnem@jfpb.jus.br>
2026-05-11 16:07:50 -0300

    refactor: remove unused app-new.html and clean up CSS for responsiveness; update footer and sidebar links

--- front/src/app/components/navbar.component.ts
+++ front/src/app/components/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -11,6 +11,12 @@ import { RouterModule } from '@angular/router';
 })
 export class NavbarComponent {
   isMenuOpen = false;
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
@@ -20,3 +26,4 @@ export class NavbarComponent {
     this.isMenuOpen = false;
   }
 }
+
